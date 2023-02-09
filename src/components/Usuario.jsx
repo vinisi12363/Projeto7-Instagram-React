b14b031a55d2a04c06fac5a798057911d93669cf
vinisi12363 <viniciuspv.si@gmail.com>
2023-02-09 00:08:58 -0300

    aidcionando requisitos do projeto no componente usuario

---
+++ src/components/Usuario.jsx
@@ -0,0 +1,26 @@
+/*
+-  O projeto também deverá ter alguns dados dinâmicos que irão mudar na tela. São eles:
+    -   Nome de usuário (ao apertar o lápis, aparece um `prompt` para alterá-lo). Se for passada uma string vazia, não atualiza.
+   
+    -  Imagem de perfil (ao clicar uma vez na imagem, aparece um `prompt` pedindo novo link). Se for passada uma string vazia, não atualiza.
+   
+    -  Salvar o post (ao clicar no ícone de salvar o post, ele deverá ficar preenchido).
+ 
+ 
+    - Caso a postagem já esteja salva, ao clicar no ícone, a postagem deve passar para não salva.
+    -   *Like* no post:
+  
+  
+    - Ao clicar no ícone do *like* o usuário poderá curtir a postagem.  Ou seja, o ícone de coração deverá ficar preenchido e vermelho.
+            - Caso a postagem já esteja curtida, ao clicar no ícone, a postagem deve passar para não curtida.
+            - Ao clicar na imagem de um post do *feed*, o usuário poderá curtir a postagem, ou seja, o botão do *like* deverá ficar preenchido em vermelho.
+ 
+        - OBS: assim como no Instagram de verdade, clicar na imagem **NÃO** retira *likes*, apenas adiciona caso já não tenha.
+
+        - Sobre a frase de “Curtido por *fulano* e outras **xxx** pessoas”:
+            - O número de *likes* deverá aumentar quando a pessoa der um *like* e diminuir quando retirá-lo.
+            - O início da frase “Curtido por *fulano*” pode ter qualquer valor no lugar de *fulano*, não precisa ser o nome do usuário logado nem mudar quando o post for curtido.
+
+
+
+*/
